11d8977bb7fd41ea181e8ce084dd904ef714dca0
Ankita practise <ankitadeb1202@gmail.com>
2023-09-20 11:30:26 +0530

    removed error

--- src/components/dashboard/category/index.jsx
+++ src/components/dashboard/category/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 // This section refer to the category section
 
-import React from 'react'
+
 
 const Category = () => (
     <div className="tab-section">
