5fa07d893f9c78884004f79edb7c8fe1695d257a
Ankita practise <ankitadeb1202@gmail.com>
2023-09-20 11:18:08 +0530

    added category section

--- src/components/dashboard/category/index.jsx
+++ src/components/dashboard/category/index.jsx
@@ -4,10 +4,33 @@ import './style.css';
 
 import React from 'react'
 
-const Category = () => {
-  return (
-    <div>index</div>
-  )
-}
+const Category = () => (
+    <div className="tab-section">
+
+        <div className="tab-item">
+            <p className="category-title">Students</p>
+            <p className="category-desc">1,049</p>
+        </div>
+
+        <div className="tab-item">
+            <p className="category-title">Sections</p>
+            <p className="category-desc">18
+                <span> in 12 classes</span>
+            </p>
+        </div>
+
+        <div className="tab-item">
+            <p className="category-title">Collection this month</p>
+            <p className="category-desc">₹90.56L</p>
+        </div>
+
+        <div className="tab-item">
+            <p className="category-title">Fine Collected till date</p>
+            <p className="category-desc">₹11.20L</p>
+        </div>
+
+    </div>
+)
+
 
 export default Category
