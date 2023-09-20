f4a002be2e2b1fb1776aec2c316ab9afda2fee48
Ankita practise <ankitadeb1202@gmail.com>
2023-09-20 11:20:18 +0530

    added disbural section with css

--- src/components/dashboard/disbural/index.jsx
+++ src/components/dashboard/disbural/index.jsx
@@ -5,7 +5,49 @@ import './style.css';
 
 const Disbural = () => {
 
-   
+    const disburals = [
+        {
+            date: "July 11, 2023", amount: "₹5,03,123", status: "Pending"
+        },
+        {
+            date: "July 10, 2023", amount: "₹5,03,123", status: "Successful"
+        },
+        {
+            date: "July 9, 2023", amount: "₹5,03,123", status: "Successful"
+        },
+        {
+            date: "July 8, 2023", amount: "₹5,03,123", status: "Successful"
+        }
+    ]
+
+    return (
+        <div className="disbural-container">
+
+            <p className="disbural-title">Disburals</p>
+            <table>
+                <thead>
+                    <tr className="bordered">
+                        <th>Date</th>
+                        <th>Amount</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {disburals.map((item, idx) => (
+                        <tr key={idx}>
+                            <td>{item.date}</td>
+                            <td>{item.amount}</td>
+                            <td className="disbural-status">
+                                <span><img src={`./assets/${item.status === "Pending" ? "pending.png" : "success.png"}`} /></span>
+                                <span>{item.status}</span>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+        </div>
+    )
 };
 
 export default Disbural;
